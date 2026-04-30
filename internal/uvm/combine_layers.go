4e0a9537fbec8e94e5fce25491f737f443e260d1
Hayato Kiwata <dev@haytok.jp>
2026-04-30 12:10:37 +0900

    fix: omit FilterType from CombinedLayers when WCIFS (regression test)
    Signed-off-by: Hayato Kiwata <dev@haytok.jp>

--- internal/uvm/combine_layers.go
+++ internal/uvm/combine_layers.go
@@ -18,15 +18,18 @@ func (uvm *UtilityVM) CombineLayersWCOW(ctx context.Context, layerPaths []hcssch
 	if uvm.operatingSystem != "windows" {
 		return errNotSupported
 	}
+	settings := guestresource.WCOWCombinedLayers{
+		ContainerRootPath: containerRootPath,
+		Layers:            layerPaths,
+	}
+	if filterType != hcsschema.WCIFS {
+		settings.FilterType = filterType
+	}
 	msr := &hcsschema.ModifySettingRequest{
 		GuestRequest: guestrequest.ModificationRequest{
 			ResourceType: guestresource.ResourceTypeCombinedLayers,
 			RequestType:  guestrequest.RequestTypeAdd,
-			Settings: guestresource.WCOWCombinedLayers{
-				ContainerRootPath: containerRootPath,
-				Layers:            layerPaths,
-				FilterType:        filterType,
-			},
+			Settings:     settings,
 		},
 	}
 	return uvm.modify(ctx, msr)
